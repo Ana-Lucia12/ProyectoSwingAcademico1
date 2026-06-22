e8a7bda1d20cf99cbd62d208a6b5e1f4d39cc261
Josue2007-ui <denisoncastillogutierrez17@gmail.com>
2026-06-21 22:36:40 -0600

    subiendo parte del integrante 1

---
+++ PProyectoSwingAcademico.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+public class RegistroDuplicadoException extends Exception {
+
+    public RegistroDuplicadoException(String mensaje) {
+        super(mensaje);
+    }
+}
