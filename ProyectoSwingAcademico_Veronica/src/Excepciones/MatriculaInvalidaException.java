5ef2f4ac77590f3fe838bc8b22ed9ff09e189f0b
verom <verom@LAPTOP-3CQUB4A9>
2026-06-21 23:40:29 -0600

    Módulo Matrícula: modelo, repositorio, negocio, panel, reglas de matrícula y control de duplicados.

---
+++ ProyectoSwingAcademico_Veronica/src/Excepciones/MatriculaInvalidaException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excepciones;
+
+/**
+ *
+ * @author verom
+ */
+public class MatriculaInvalidaException extends Exception{
+    
+    public MatriculaInvalidaException( String mensaje) {
+
+        super(mensaje);
+        
+    }
+}
